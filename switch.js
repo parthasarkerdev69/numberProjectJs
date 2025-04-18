cbbd28605fc68c39b39742bb6ce2e8fdb02ed77f
parthasarkerdev69 <parthasarker252@gmail.com>
2025-04-18 18:13:55 +0600

    fizzBuzz

---
+++ switch.js
@@ -0,0 +1,57 @@
+// var num = "99";
+
+// switch(num){
+//     case"10":
+//         console.log("wow");
+//         break;
+//     case"50":
+//         console.log("milse");
+//         break;
+//     case"70":
+//         console.log("na");
+//         break;
+//     case"80":
+//         console.log("ki");
+//         break;
+//     case"69":
+//         console.log("durr");
+//         break;
+//     default : console.log("ami jani na ");
+// }
+
+// vowel
+
+// let letter = "A";
+// letter = letter.toLocaleLowerCase();
+
+// switch(letter){
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         console.log("vowel");
+//         break;
+//     default:
+//         console.log("consonant");
+// }
+
+
+// let sum = 0;
+// let i =1; 
+//  while(i <= 100){
+   
+//   if( i%3 === 0 && i%5 === 0){
+//     console.log(i);
+//     sum = sum + i;
+    
+//   }
+//     i= i+1;
+//  }
+//  console.log(sum);
+ 
+ let i = 1;
+ do{
+    console.log(i);
+    i++
+ }while(i <= 100)
